1396819d90c9a9ee39c8f9ebb551d9ab60bdafed
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-05 20:19:05 +0300

    First basic ASP.Net working project -Controller, ActionResult, View working logic
    http://localhost:50176/department/test
    http://localhost:50176/department/hello

---
+++ PersonnelMVC/PersonnelMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using PersonnelMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonnelMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public ActionResult Hello()
+        {
+            var department = new Department() { Id = 1, Name = "Computing" };
+            return View(department);
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
