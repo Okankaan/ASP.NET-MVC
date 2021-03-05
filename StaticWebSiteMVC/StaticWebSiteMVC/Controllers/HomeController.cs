4c33ef45baf1064f9a0740bd83f8849cf6a50b01
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-05 23:58:18 +0300

    Static WebSite Template Design and Routing with using Asp.Net MVC

---
+++ StaticWebSiteMVC/StaticWebSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("products")]
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        [Route("store")]
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}
