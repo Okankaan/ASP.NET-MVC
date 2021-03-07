9d819bc38f7da53f04ac7d5ae8c5ac517988a57e
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-08 01:25:17 +0300

    Closed Lazy Loading and used Eager Loading. In Eager Loading, tables will join in query and just one query goes to database. -using for performance improvement

--- PersonnelMVCUI/PersonnelMVCUI/Controllers/PersonnelController.cs
+++ PersonnelMVCUI/PersonnelMVCUI/Controllers/PersonnelController.cs
@@ -1,6 +1,7 @@
 ﻿using PersonnelMVCUI.Models.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,8 +14,8 @@ namespace PersonnelMVCUI.Controllers
         // GET: Personnel
         public ActionResult Index()
         {
-            var model = db.Personnel.ToList();
-            return View(model);
+            var model = db.Personnel.Include(x => x.Department).ToList();//Lazy loading closed and Eager Loading using. In Eager Loading, Tables will join like this.
+            return View(model);                                        //With Eager Loading(Joining), just one query goes to database.
         }
     }
 }
--- PersonnelMVCUI/PersonnelMVCUI/Models/EntityFramework/PersonnelModel.Context.cs
+++ PersonnelMVCUI/PersonnelMVCUI/Models/EntityFramework/PersonnelModel.Context.cs
@@ -18,6 +18,7 @@ namespace PersonnelMVCUI.Models.EntityFramework
         public PersonnelDbEntities()
             : base("name=PersonnelDbEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
