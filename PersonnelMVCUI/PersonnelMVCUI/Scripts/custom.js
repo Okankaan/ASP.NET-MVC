29876dbbc89e4113b31106a1c89946591a080a09
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-08 23:46:07 +0300

    Deletion process with using Ajax and using bootbox

--- PersonnelMVCUI/PersonnelMVCUI/Scripts/custom.js
+++ PersonnelMVCUI/PersonnelMVCUI/Scripts/custom.js
@@ -1,4 +1,22 @@
-﻿function CheckDateTypeIsValid(dateElement) {
+﻿$(function () {
+    $("#tblDepartments").on("click", ".btnDeleteDepartment", function () {
+        var btn = $(this);
+        bootbox.confirm("Are you sure you want to delete the department?", function (result) {
+            if (result) {
+                var id = btn.data("id");
+                $.ajax({
+                    type: "GET",
+                    url: "/Department/Delete/" + id,
+                    success: function () {
+                        btn.parent().parent().remove();
+                    }
+                });
+            }
+        });
+    });
+});
+
+function CheckDateTypeIsValid(dateElement) {
     var value = $(dateElement).val();
     if (value == '') {
         $(dateElement).valid("false");
