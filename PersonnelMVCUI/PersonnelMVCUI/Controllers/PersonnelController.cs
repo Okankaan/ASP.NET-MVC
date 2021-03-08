08264768da9313d42f3fbf4f240648619cf6a7c0
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-08 21:52:44 +0300

    Personnel Add, update and delete

--- PersonnelMVCUI/PersonnelMVCUI/Controllers/PersonnelController.cs
+++ PersonnelMVCUI/PersonnelMVCUI/Controllers/PersonnelController.cs
@@ -1,4 +1,5 @@
 ﻿using PersonnelMVCUI.Models.EntityFramework;
+using PersonnelMVCUI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -16,6 +17,51 @@ namespace PersonnelMVCUI.Controllers
         {
             var model = db.Personnel.Include(x => x.Department).ToList();//Lazy loading closed and Eager Loading using. In Eager Loading, Tables will join like this.
             return View(model);                                        //With Eager Loading(Joining), just one query goes to database.
+        }
+
+        public ActionResult New()
+        {
+            var model = new PersonnelFormViewModel()
+            {
+                Departments = db.Department.ToList()
+            };
+            return View("PersonnelForm", model);
+        }
+
+        public ActionResult Save(Personnel personnel)
+        {
+            if (personnel.Id == 0)//Add
+            {
+                db.Personnel.Add(personnel);
+            }
+            else//Update
+            {
+                db.Entry(personnel).State = System.Data.Entity.EntityState.Modified;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var model = new PersonnelFormViewModel()
+            {
+                Departments = db.Department.ToList(),
+                Personnel = db.Personnel.Find(id)
+            };
+            return View("PersonnelForm", model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var personnelToDeleted = db.Personnel.Find(id);
+            if (personnelToDeleted == null)
+            {
+                return HttpNotFound();
+            }
+            db.Personnel.Remove(personnelToDeleted);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ PersonnelMVCUI/PersonnelMVCUI/ViewModels/PersonnelFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PersonnelMVCUI.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonnelMVCUI.ViewModels
+{
+    public class PersonnelFormViewModel
+    {
+        public IEnumerable<Department> Departments { get; set; }
+        public Personnel Personnel { get; set; }
+    }
+}
