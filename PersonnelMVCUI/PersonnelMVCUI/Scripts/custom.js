084a4fc84c3a76fb05580be92cc1442b5bf5f7d6
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-08 23:23:29 +0300

    Date Type validation control with custom.js

---
+++ PersonnelMVCUI/PersonnelMVCUI/Scripts/custom.js
@@ -0,0 +1,9 @@
+﻿function CheckDateTypeIsValid(dateElement) {
+    var value = $(dateElement).val();
+    if (value == '') {
+        $(dateElement).valid("false");
+    }
+    else {
+        $(dateElement).valid();
+    }
+}
