34b6c0b4775ca0ce5fb08589f27fba07e3d0633e
Okankaan <okankaan.cetinkaya@gmail.com>
2021-03-10 23:48:04 +0300

    BundleConfig - Optimization and minify to Javascript and Css files

---
+++ PersonnelMVCUI/PersonnelMVCUI/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PersonnelMVCUI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-3.0.0.min.js",
+                "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/custom.js",
+                "~/Scripts/bootbox.js",
+                "~/Scripts/DataTables/jquery.dataTables.min.js",
+                "~/Scripts/DataTables/dataTables.bootstrap.min.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/site.css",
+                "~/Content/DataTables/css/dataTables.bootstrap.min.css"
+                ));
+        }
+    }
+}
--- PersonnelMVCUI/PersonnelMVCUI/Global.asax.cs
+++ PersonnelMVCUI/PersonnelMVCUI/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using PersonnelMVCUI.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace PersonnelMVCUI
@@ -14,6 +16,7 @@ namespace PersonnelMVCUI
             GlobalFilters.Filters.Add(new AuthorizeAttribute());//Authorize control at application level
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
